f93143735abc097130ab332a267443e7b56acc50
Mike Pratt <mike@msjp.me>
2021-06-28 16:18:59 +0100

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex flex-col bg-gray-50">
+      <header className="flex flex-row w-full p-4">
+        <h1 className="text-5xl text-red-500">Pomodoro</h1>
+        <div className="flex flex-1 justify-end space-x-4">
+          <button className="bg-red-500 hover:bg-red-400 text-gray-50 shadow rounded p-2">Restore previous session</button>
+          <button className="bg-red-500 hover:bg-red-400 text-gray-50 shadow rounded p-2">Clear session</button>
+        </div>
       </header>
+      <main className="flex h-full w-full bg-gray-50">
+        
+      </main>
     </div>
   );
 }
