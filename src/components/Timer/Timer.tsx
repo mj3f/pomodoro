565a6ada6f5589eb4863221e4e4dcf3c65088955
Mike Pratt <mike@msjp.me>
2021-06-29 10:21:27 +0100

    Added countdown timer implementation. Fixed bugs with pausing/playing the countdown timer. Fixed flexbox issues causing the timer to grow/shrink when the value changes.

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,37 +1,70 @@
 import { faPause, faPlay, faSync } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import SessionChanger from '../SessionChanger/SessionChanger';
-
-let countdown: NodeJS.Timeout;
-
-function startTimer(seconds: number) {
-    const now = Date.now();
-    const then = now + seconds * 1000;
-    countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now())) / 1000;
-        if (secondsLeft < 0) {
-            clearInterval(countdown);
-            return;
-        }
-        console.log(secondsLeft);
-    }, 1000);
-}
-
-function formatTimerCountdown(duration: number): string {
-    return '';
-}
 
 export default function Timer() {
     const [breakLength, setBreakLength] = useState(5);
     const [sessionLength, setSessionLength] = useState(25);
-    const [countdown, setCountdown] = useState('25:00');
+    const [countdownValues, setCountdownValue] = useState<CountdownValues>({
+        output: '25:00'
+    });
     const [isPlaying, setIsPlaying] = useState(false);
-    // startTimer(10);
+
+    let countdownTimeout = useRef<NodeJS.Timeout>(null);
+
+    useEffect(() => {
+        if (isPlaying) {
+            countdownValues.minutes && countdownValues.seconds ?
+                startCountdown(countdownValues.minutes, countdownValues.seconds) :
+                startCountdown(sessionLength);
+        } else {
+            // @ts-ignore
+            clearTimeout(countdownTimeout.current);
+        }
+    }, [isPlaying]);
+
+    useEffect(() => {
+        return () => { // Clear timeout on unmounting of component.
+            // @ts-ignore
+            clearTimeout(countdownTimeout.current);
+        }
+    }, []);
+
+
+    const startCountdown = (minutes: number, seconds?: number) => {
+        let secs = seconds ? seconds : 60;
+        let mins = minutes;        
+
+        const tick = () => {
+            if (!isPlaying) {
+                return;
+            }
+
+            const currentMins = mins - 1; // decrement minutes, then decrement each second until seconds is 0.
+            secs--;
+
+            setCountdownValue({
+                output: (currentMins < 10 ? '0' : '') + currentMins.toString() + ':' + (secs < 10 ? '0' : '') + String(secs),
+                minutes: mins,
+                seconds: secs
+            });
+
+            if (secs > 0) {
+                // @ts-ignore
+                countdownTimeout.current = setTimeout(() => tick(), 1000);
+            } else {
+                if (mins > 1) {
+                    startCountdown(mins - 1); // repeat above steps for the next minute in the countdown.
+                }
+            }
+        }
+        tick();
+    };
 
     const setSession = (value: number) => {
         setSessionLength(value);
-        setCountdown(`${value < 10 ? '0' : ''}${value}:00`);
+        setCountdownValue({ output: `${value < 10 ? '0' : ''}${value}:00` });
     }
 
     const setPlayOrPause = () => {
@@ -42,7 +75,7 @@ export default function Timer() {
         <div className="flex flex-col justify-center items-center w-full">
             <div className="flex flex-row w-full justify-center items-center">
                 <SessionChanger title="Session Length" value={sessionLength} onValueChange={setSession} className="flex-1" />
-                <h3 className="text-8xl font-semibold text-red-500">{countdown}</h3>
+                <h3 className="text-8xl font-semibold text-red-500 flex-1 text-center">{countdownValues.output}</h3>
                 <SessionChanger title="Break Length" value={breakLength} onValueChange={setBreakLength} className="flex-1" />
             </div>
             <div className="flex flex-1 w-full items-center justify-center space-x-4">
@@ -56,4 +89,10 @@ export default function Timer() {
         </div>
         
     );
+}
+
+interface CountdownValues {
+    output: string,
+    minutes?: number,
+    seconds?: number
 }
