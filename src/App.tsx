7296a86aeca648cddbfa4d526016db619c8ddcfa
Mike Pratt <mike@msjp.me>
2021-06-30 16:57:28 +0100

    Fixed indenting in App.tsx (again).

--- src/App.tsx
+++ src/App.tsx
@@ -55,14 +55,13 @@ function App() {
 				className="fixed inset-0 z-10 overflow-y-auto"
 				onClose={() => setModalIsOpen(false)}>
 				<div className="min-h-screen px-4 text-center">
-					<Transition.Child 	as={Fragment}
-										enter="ease-out duration-300"
-										enterFrom="opacity-0"
-										enterTo="opacity-100"
-										leave="ease-in duration-200"
-										leaveFrom="opacity-100"
-										leaveTo="opacity-0"
-					>
+					<Transition.Child as={Fragment}
+									  enter="ease-out duration-300"
+									  enterFrom="opacity-0"
+									  enterTo="opacity-100"
+									  leave="ease-in duration-200"
+									  leaveFrom="opacity-100"
+									  leaveTo="opacity-0">
 						<Dialog.Overlay className="fixed inset-0" />
 					</Transition.Child>
 
